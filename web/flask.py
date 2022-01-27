b53ac1a4f12fe19fcc36db276d9b713af4bbb57f
Ubuntu <oleg.medovikov@mail.ru>
2022-01-27 10:18:58 +0000

    new

--- web/flask.py
+++ web/flask.py
@@ -9,17 +9,25 @@ app = Flask(__name__)
 async def index():
     ID = request.args.get('id')
     identificator = await get_identificator(int(ID))
+    # Если неправильный id в адресе, то выдаём ошибку
     if identificator == 0:
         return render_template(
                 'error_no_id.html',
                 name='error',
                 )
+    # Ищем имя и фамилию юзера в базе
+    sql = f"""select first_name,second_name
+                from groups
+                    where user_id={int(ID)}"""
 
-    sql = f"select first_name, second_name from groups where user_id={int(ID)}"
-    print(sql)
     res = await DB_QUERY(sql)
-    print(res)
     username = str(res[0][0]) + ' ' +  str(res[0][1])
+    
+    #if request.metod == 'POST':
+    #    SNILS = request.form.get('snils')
+        
+
+    # отправляем форму приветсвия
     return render_template(
             'index.html',
             name='main',
