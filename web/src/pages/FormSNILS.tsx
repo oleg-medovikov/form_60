3bf90310a615e02728219e74978d77933f29ce45
Roman Silivestrov <roman.silivestrov@metalamp.io>
2022-02-03 20:47:11 +0300

    Add @mui/material, react-hook-form

--- web/src/pages/FormSNILS.tsx
+++ web/src/pages/FormSNILS.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import TextField from '@mui/material/TextField';
 
 type Props = {};
 
@@ -7,6 +9,8 @@ const FormSNILS: React.FC<Props> = () => {
   const [searchParams] = useSearchParams();
   const [userID, setUserID] = useState('');
   const [userFIO, setUserFIO] = useState('');
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data: any) => console.log(data);
 
   useEffect(() => {
     const getData = async () => {
@@ -25,12 +29,24 @@ const FormSNILS: React.FC<Props> = () => {
     getData();
   }, []);
 
-  return userFIO === '' ? (
-    <h1>Loading...</h1>
-  ) : (
-    <h1>
-      Привет, {userFIO}! Ваш ID: {userID}
-    </h1>
+  return (
+    <>
+      {userFIO === '' ? (
+        <h1>Loading...</h1>
+      ) : (
+        <h1>
+          Привет, {userFIO}! Ваш ID: {userID}
+        </h1>
+      )}
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <TextField
+          type="search"
+          variant="outlined"
+          label="СНИЛС"
+          {...register('snils', { required: true })}
+        />
+      </form>
+    </>
   );
 };
 
