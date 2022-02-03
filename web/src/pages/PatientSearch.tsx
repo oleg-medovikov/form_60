25722b108baf550e73010bf5369ac36a39996129
Roman Silivestrov <roman.silivestrov@metalamp.io>
2022-02-03 23:31:19 +0300

    Rename FormSNILS to PatientSearch; add snils mask

--- web/src/pages/PatientSearch.tsx
+++ web/src/pages/PatientSearch.tsx
@@ -1,16 +1,34 @@
 import { useEffect, useState } from 'react';
-import { useSearchParams } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
+import { useSearchParams } from 'react-router-dom';
+import Button from '@mui/material/Button';
+import Input from '@mui/material/Input';
 import TextField from '@mui/material/TextField';
+import styled from '@emotion/styled';
 
 type Props = {};
 
-const FormSNILS: React.FC<Props> = () => {
+const StyledForm = styled.form`
+  display: flex;
+  gap: 4px;
+`;
+
+const PatientSearch: React.FC<Props> = () => {
   const [searchParams] = useSearchParams();
   const [userID, setUserID] = useState('');
   const [userFIO, setUserFIO] = useState('');
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit, setValue } = useForm();
   const onSubmit = (data: any) => console.log(data);
+
+  const onSnilsChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const masked = e.target.value
+      .replace(/\D/g, '')
+      .replace(/^(\d{3})-?(\d{1,3})$/, '$1-$2')
+      .replace(/^(\d{3})-?(\d{3})-?(\d{1,3})$/, '$1-$2-$3')
+      .replace(/^(\d{3})-?(\d{3})-?(\d{3})\s?(\d{1,2})(\d+)?$/, '$1-$2-$3 $4');
+
+    setValue('snils', masked);
+  };
 
   useEffect(() => {
     const getData = async () => {
@@ -20,6 +38,7 @@ const FormSNILS: React.FC<Props> = () => {
         const { user_id: id, first_name: firstName, second_name: secondName } = await res.json();
         setUserID(id);
         setUserFIO(`${firstName} ${secondName}`);
+        setValue('identificator', id)
       } catch (err) {
         setUserID('0');
         setUserFIO('Аноним');
@@ -38,16 +57,20 @@ const FormSNILS: React.FC<Props> = () => {
           Привет, {userFIO}! Ваш ID: {userID}
         </h1>
       )}
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <StyledForm onSubmit={handleSubmit(onSubmit)}>
         <TextField
           type="search"
           variant="outlined"
           label="СНИЛС"
-          {...register('snils', { required: true })}
+          {...register('snils', { required: true, onChange: onSnilsChange })}
         />
-      </form>
+        <Input type="hidden" {...register('identificator', { required: true })} />
+        <Button type="submit" variant="contained">
+          Искать
+        </Button>
+      </StyledForm>
     </>
   );
 };
 
-export { FormSNILS };
+export { PatientSearch };
