6a6aacb4c12849acdf86f6fa6e51e8815ad51bfd
Ubuntu <oleg.medovikov@mail.ru>
2022-02-03 10:24:41 +0000

    slil

--- web/src/pages/FormSNILS.tsx
+++ web/src/pages/FormSNILS.tsx
@@ -12,8 +12,9 @@ const FormSNILS: React.FC<Props> = () => {
     const getData = async () => {
       try {
         const uid = searchParams.get('uid');
-        const res = await fetch(`/users/${uid}`);
+        const res = await fetch(`https://xn--b1aaedrmjsd.xn--p1ai:8443/users/${uid}`);
         const { user_id: id, first_name: firstName, second_name: secondName } = await res.json();
+
         setUserID(id);
         setUserFIO(`${firstName} ${secondName}`);
       } catch (err) {
