c30dd26531ea26d7b709c9ea477087009b883743
Roman Silivestrov <roman.silivestrov@metalamp.io>
2022-02-13 12:42:05 +0300

    Add Container to PatientForm

--- web/src/components/PatientForm/PatientForm.tsx
+++ web/src/components/PatientForm/PatientForm.tsx
@@ -1,9 +1,9 @@
 import { useForm } from 'react-hook-form';
 import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
 import TextField from '@mui/material/TextField';
 
 import { Form } from '@components/Form';
-import { FormGroup } from '@mui/material';
 
 type Props = {
   onSubmit: (data: { [key: string]: string }) => void;
@@ -17,96 +17,96 @@ const PatientForm: React.FC<Props> = ({ onSubmit }) => {
   } = useForm();
 
   return (
-    <Form onSubmit={handleSubmit(onSubmit)}>
-      <TextField
-        type="text"
-        variant="outlined"
-        label="ФИО"
-        {...register('fio', { required: true })}
-        error={!!errors.fio}
-        helperText={false}
-        margin="normal"
-      />
-      <TextField
-        type="text"
-        variant="outlined"
-        label="УНРЗ"
-        {...register('unrz', { required: true })}
-        error={!!errors.unrz}
-        helperText={false}
-        margin="normal"
-      />
-      <TextField
-        type="text"
-        variant="outlined"
-        label="Полис"
-        {...register('polis', { required: true })}
-        error={!!errors.polis}
-        helperText={false}
-        margin="normal"
-      />
-      <TextField
-        type="text"
-        variant="outlined"
-        label="Эпидномер"
-        {...register('epidNumber', { required: true })}
-        error={!!errors.epidNumber}
-        helperText={false}
-        margin="normal"
-      />
-      <TextField
-        type="text"
-        variant="outlined"
-        label="Паспорт"
-        {...register('passport', { required: true })}
-        error={!!errors.passport}
-        helperText={false}
-        margin="normal"
-      />
-      <TextField
-        type="text"
-        variant="outlined"
-        label="Телефон"
-        {...register('phone', { required: true })}
-        error={!!errors.phone}
-        helperText={false}
-        margin="normal"
-      />
-      <TextField
-        type="text"
-        variant="outlined"
-        label="Адрес"
-        {...register('address', { required: true })}
-        error={!!errors.address}
-        helperText={false}
-        margin="normal"
-      />
-      <FormGroup>
-        <span>1</span>
-        <span>2</span>
-      </FormGroup>
-      <TextField
-        type="text"
-        variant="outlined"
-        label="Врач"
-        {...register('doctor', { required: true })}
-        error={!!errors.doctor}
-        helperText={false}
-        margin="normal"
-      />
-      <TextField
-        type="text"
-        variant="outlined"
-        label="Место работы"
-        {...register('workplace', { required: true })}
-        error={!!errors.workplace}
-        helperText={false}
-        margin="normal"
-      />
-      <Button type="submit" variant="contained" sx={{ mt: 2 }}>
-        Сохранить
-      </Button>
-    </Form>
+    <Container sx={{ py: 6 }}>
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        <TextField
+          type="text"
+          variant="outlined"
+          label="ФИО"
+          {...register('fio', { required: true })}
+          error={!!errors.fio}
+          helperText=" "
+          margin="normal"
+        />
+        {/* Birth date, register date */}
+        <TextField
+          type="text"
+          variant="outlined"
+          label="УНРЗ"
+          {...register('unrz', { required: true })}
+          error={!!errors.unrz}
+          helperText=" "
+          margin="normal"
+        />
+        <TextField
+          type="text"
+          variant="outlined"
+          label="Полис"
+          {...register('polis', { required: true })}
+          error={!!errors.polis}
+          helperText=" "
+          margin="normal"
+        />
+        <TextField
+          type="text"
+          variant="outlined"
+          label="Эпидномер"
+          {...register('epidNumber', { required: true })}
+          error={!!errors.epidNumber}
+          helperText=" "
+          margin="normal"
+        />
+        <TextField
+          type="text"
+          variant="outlined"
+          label="Паспорт"
+          {...register('passport', { required: true })}
+          error={!!errors.passport}
+          helperText=" "
+          margin="normal"
+        />
+        <TextField
+          type="text"
+          variant="outlined"
+          label="Телефон"
+          {...register('phone', { required: true })}
+          error={!!errors.phone}
+          helperText=" "
+          margin="normal"
+        />
+        <TextField
+          type="text"
+          variant="outlined"
+          label="Адрес"
+          {...register('address', { required: true })}
+          error={!!errors.address}
+          helperText=" "
+          margin="normal"
+        />
+        {/* Start date, end date */}
+        <TextField
+          type="text"
+          variant="outlined"
+          label="Врач"
+          {...register('doctor', { required: true })}
+          error={!!errors.doctor}
+          helperText=" "
+          margin="normal"
+        />
+        <TextField
+          type="text"
+          variant="outlined"
+          label="Место работы"
+          {...register('workplace', { required: true })}
+          error={!!errors.workplace}
+          helperText=" "
+          margin="normal"
+        />
+        <Button type="submit" variant="contained" sx={{ mt: 2 }}>
+          Сохранить
+        </Button>
+      </Form>
+    </Container>
   );
 };
 
